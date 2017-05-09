9cb31c8fd0ffa35b2b406044dba880033b946e9b
maximlus <turtaljack@gmail.com>
2017-05-09 13:03:13 +0100

    website draft
    First commit

---
+++ script/home.js
@@ -0,0 +1,44 @@
+function main() {
+    console.log("in main function");
+    var myform = document.getElementById("login");
+    myform.addEventListener("submit",validateForm);
+    
+    var sizeForm = document.getElementById("size");
+    sizeForm.addEventListener("change", priceAdjust;
+}
+
+function validateForm(event) {
+    var nameUser = document.getElementById("userName");
+    var wordPass = document.getElementById("passWord");
+    var formValid = true;
+    var myForm = document.getElementById("login");
+    
+    if (myForm.userName.value == "") {
+        formValid = false;
+        nameUser.placeholder = "Required";
+        //nameUser.style.placeholder.color = "#ff0000";
+        event.preventDefault();
+    } else {
+        nameUser.placeholder = "Name";
+    }
+    if (myForm.passWord.value == "") {
+        formValid = false;
+        wordPass.placeholder = "Required";
+        //wordPass.style.placeholder.color = "#ff0000";
+        event.preventDefault();
+    } else {
+        wordPass.placeholder = "Password";
+    }
+  
+}
+
+function priceAdjust(event){
+    var x=0
+    console.log("the form has changed");
+        var form = document.getElementById("size");
+        for(var i=0; i<form.size.length; i++){
+            if(form.size[i].checked){
+                console.log("you have selected" + form.extras[i].value);
+            }
+        }
+}
