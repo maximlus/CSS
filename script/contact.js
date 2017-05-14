9f90b9c485d6ac30306620ebf181d508ceafd3de
maximlus <turtaljack@gmail.com>
2017-05-14 20:51:34 +0100

    Compleation of all Core
    All core sections completed. Begin work on intermidiate work. I hope you
    enjoy reading theses notes.
    Dose it get boring reading all of theses for all your pupals?

--- script/contact.js
+++ script/contact.js
@@ -1,17 +1,63 @@
 function main() {
-	console.log("hello");
+    console.log("in main function");
+	console.log("If you're looking at this, it means something has gone wrong. sad times.");
+	/*googleMap();*/
+	
 }
-function initMap() {
-	var uluru = {lat: -25.363, lng: 131.044};
+/*
+function googleMap() {
+	var uluru = {lat: 50.9080, lng: -1.4002};
 	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 4,
+		zoom: 16,
 		center: uluru
 	});
 	var marker = new google.maps.Marker({
 		position: uluru,
 		map: map
 	});
-}
-function async defer(){
-	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCYT1gBgN1a7Q7B0rrsyNKyirwnXdQPi54&callback=initMap">
+}*/
+function formvalid(){
+	console.log("I have been called");
+	var x = 1; /* used to determin weather the form is valid*/
+	if(document.getElementById("firstName").value == ''){
+		document.getElementById("fError").style.display = "block";
+		console.log("First name is missing.");
+		x = 0;
+	}
+	else{
+		console.log("First name is fine")
+		document.getElementById("fError").style.display = "none";
+		if(document.getElementById("lastName").value == ''){
+			document.getElementById("lError").style.display = "block";
+			console.log("Last name is missing");
+			event.preventDefault();
+			x = 0;
+		}
+		else{
+			console.log("Last name is fine")
+			document.getElementById("lError").style.display = "none";
+			if(document.getElementById("e-mail").value == ''){
+				document.getElementById("eError").style.display = "block";
+				console.log("email is missing");
+				event.preventDefault();
+				x = 0;
+			}
+		else{
+			console.log("email is fine")
+			if(document.getElementById("message").value == ''){
+				document.getElementById("mError").style.display = "block";
+				console.log("message is missing");
+				event.preventDefault();
+				x = 0;
+	}
+	else{
+		console.log("everything is fine")
+		return true;
+		}
+		}
+		}
+	}
+	if (x == 0){
+		event.preventDefault();
+	}
 }
